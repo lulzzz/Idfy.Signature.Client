cdce40f2bdbda84d66c39710cd9de4223864dcca
Rune Synnevåg <runes@unipluss.no>
2017-10-25 08:29:18 +0200

    Added meta data dictionary

--- src/Idfy.Signature.Client/Idfy.Signature.Models/Documents/CreateDocumentResponse.cs
+++ src/Idfy.Signature.Client/Idfy.Signature.Models/Documents/CreateDocumentResponse.cs
@@ -8,5 +8,10 @@ namespace Idfy.Signature.Models.Documents
     {
         public Guid DocumentId { get; set; }
         public List<SignerResponse> Signers { get; set; }
+
+        /// <summary>
+        /// A dicitonary with extra information from additional services. See developer documentation for more information
+        /// </summary>
+        public Dictionary<string, string> MetaData { get; set; }
     }
 }
