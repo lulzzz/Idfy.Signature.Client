50ad7b252c3201ba94a6251092ad5b8aefe9ddee
Aleksander Sleire <Alekken@live.no>
2017-11-08 14:32:04 +0100

    read test config from file in debug

--- src/Idfy.Signature.Client/Idfy.Signature.Client.Test/Config.cs
+++ src/Idfy.Signature.Client/Idfy.Signature.Client.Test/Config.cs
@@ -1,25 +1,37 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Text;
 
 namespace Idfy.Signature.Client.Test
 {
     public static class Config
     {
-        public static Dictionary<string, string> debugConfig = new Dictionary<string, string>()
+        private static Dictionary<string, string> _dict = null;
+        public static Dictionary<string, string> ConfigDictionary
         {
-            { "AccountId", "INSERT_VALUE" },
-            { "ClientId", "INSERT_VALUE" },
-            { "ClientSecret", "INSERT_VALUE" },
-            { "Scope", "INSERT_VALUE" },
-            { "IsProd", "INSERT_VALUE" },
-        };
+            get
+            {
+                try
+                {
+                    if (_dict == null)
+                    {
+                        _dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("testcfg.json"));
+                    }
+                    return _dict;
+                } catch(Exception e)
+                {
+                    throw new Exception("Failed to load config. Create a \"testcfg.json\" file in working directory", e);
+                }
+            }
+        }
 
         public static string Get(string key)
         {
 #if DEBUG
-            return debugConfig[key];
+            return ConfigDictionary[key];
 #else
             return Environment.GetEnvironmentVariable("TESTCFG_" + key);
 #endif
