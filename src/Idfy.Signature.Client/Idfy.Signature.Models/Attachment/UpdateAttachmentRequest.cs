08120f3c2fc4461912b16257872405c52b6fead8
vPolland <vegard.polland@unipluss.no>
2017-11-16 13:17:48 +0100

    Added attachment models

---
+++ src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Idfy.Signature.Models.Misc;
+
+namespace Idfy.Signature.Models.Attachment
+{
+    public class UpdateAttachmentRequest : AttachmentRequest
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class AttachmentListItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public AttachmentType Type { get; set; }
+    }
+
+}
