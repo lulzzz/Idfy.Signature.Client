bb1d774978748065aed58052948dd977c81747d8
Aleksander Sleire <Alekken@live.no>
2017-11-17 12:08:31 +0100

    added filename to AttachmentListItem

--- src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
+++ src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
@@ -14,6 +14,7 @@ namespace Idfy.Signature.Models.Attachment
         public string Title { get; set; }
         public string Description { get; set; }
         public AttachmentType Type { get; set; }
+        public string FileName { get; set; }
     }
 
 }
