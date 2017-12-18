44988c2e75b8d17dad366e66a8007750f45cce79
Christoffer Slettan <christofferslettan@gmail.com>
2017-12-18 12:29:21 +0100

    Remove Id from UpdateAttachmentRequest

---
+++ src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/AttachmentListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Idfy.Signature.Models.Misc;
+
+namespace Idfy.Signature.Models.Attachment
+{
+    public class AttachmentListItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public AttachmentType Type { get; set; }
+        public string FileName { get; set; }
+    }
+}
--- src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
+++ src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using Idfy.Signature.Models.Misc;
-
-namespace Idfy.Signature.Models.Attachment
+﻿namespace Idfy.Signature.Models.Attachment
 {
     public class UpdateAttachmentRequest : AttachmentRequest
     {
-        public Guid Id { get; set; }
     }
-
-    public class AttachmentListItem
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public AttachmentType Type { get; set; }
-        public string FileName { get; set; }
-    }
-
 }
