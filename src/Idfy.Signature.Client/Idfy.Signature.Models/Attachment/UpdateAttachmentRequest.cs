df4e3f24c66b261f927b272ea6112becb3013c96
vPolland <vegard.polland@unipluss.no>
2018-01-18 12:44:42 +0100

    Separated update attchemnt from attachment request

--- src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
+++ src/Idfy.Signature.Client/Idfy.Signature.Models/Attachment/UpdateAttachmentRequest.cs
@@ -1,28 +1,41 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Idfy.Signature.Models.Misc;
 
 namespace Idfy.Signature.Models.Attachment
 {
-    public class UpdateAttachmentRequest : AttachmentRequest
+    public class UpdateAttachmentRequest
     {
         /// <summary>
         /// Filename with file extension. <span style="color:red;">We only support PDF for attachments</span>
         /// </summary>
-        public new string FileName { get; set; }
+        public  string FileName { get; set; }
         /// <summary>
         /// Give the attachment a title that will be presented to the user
         /// </summary>
-        public new string Title { get; set; }
+        public  string Title { get; set; }
         /// <summary>
         /// base 64 encoded attachment (utf-8)
         /// </summary>
-        public new string Data { get; set; }
+        /// <summary>
+        /// Optional: Specify which signers that should be able to see / sign this attachment
+        /// </summary>
+        public IList<Guid> Signers { get; set; }
+        /// <summary>
+        /// Optional: An optional description of the document
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// base 64 encoded attachment (utf-8)
+        /// </summary>
+        public string Data { get; set; }
         /// <summary>
         /// Choose between the following:
         /// * <b>show_accept:</b> The signer will see the attachment before signing the main document (is default now)
         /// * <b>read_accept:</b> The signer have to see the entire document before they can continue, 
         /// * <b>sign:</b> The signer has to sign the attachment (extra cost per signature)
         /// </summary>
-        public new AttachmentType? Type { get; set; }
+        public AttachmentType? Type { get; set; }
     }
 }
